e694e48ed6dbf3d06efc9741b0a378fb81cf705e
Agnia Albaitsah <agnialatas1@gmail.com>
2025-08-08 14:10:58 +0700

    avv

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+export default function Home() {
+return <div className="flex items-center justify-center h-screen">
+   <Button className="bg-blue-500">clic</Button>
+</div>
+
+}
